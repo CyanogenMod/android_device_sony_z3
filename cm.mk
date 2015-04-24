c3d858b345c040e42769c3c4f6bbba487eef8441
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2015-04-24 18:29:27 +0200

    update signature
    Change-Id: Ib575207a33f805328dbd0d8a77a8acd6010a701d

--- cm.mk
+++ cm.mk
@@ -7,8 +7,8 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 $(call inherit-product, device/sony/z3/full_z3.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D6603
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D6603/D6603:4.4.4/23.0.A.2.93/ovf_Rw:user/release-keys
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D6603-user 4.4.4 23.0.A.2.93 ovf_Rw release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D6603/D6603:5.0.2/23.1.A.0.690/814068233:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D6603-user 5.0.2 23.1.A.0.690 814068233 release-keys"
 
 PRODUCT_NAME := cm_z3
 PRODUCT_DEVICE := z3
